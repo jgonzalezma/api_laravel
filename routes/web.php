eaa1aaee61c452ec93ad2d4d78fb74785d4348a8
jgonzalezma <jgonzalezma17dw@ikzubirimanteo.com>
2019-01-30 09:43:11 +0100

    autenticacion con token

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/planets', 'SWController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
